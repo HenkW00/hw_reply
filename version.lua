f570e9039c84e5ce08312b59ad7df70e11d27b1b
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-04-22 15:00:01 +0200

    Fixed version error

--- version.lua
+++ version.lua
@@ -1,111 +1,90 @@
 local curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
-local isBought = true
-local resourceName = 'hw_reply'
-local authPrinted = false
+local resourceName = "hw_reply"
 
-CreateThread(function()
-    while true do
-        PerformHttpRequest("https://api.github.com/repos/HenkW00/hw_reply/releases/latest", CheckVersion, "GET")
-        Wait(3500000)
-    end
-end)
-
-CheckVersion = function(err, responseText, headers)
-    local repoVersion, repoURL, repoBody = GetRepoInformations()
-
+if Config.checkForUpdates then
     CreateThread(function()
-        if curVersion ~= repoVersion then
-            Wait(4000)
-            print("^0[^3WARNING^0] ^5" .. resourceName .. "^0 is ^1NOT ^0up to date!")
-            print("^0[^3WARNING^0] Your Version: ^2" .. curVersion .. "^0")
-            print("^0[^3WARNING^0] Latest Version: ^2" .. repoVersion .. "^0")
-            print("^0[^3WARNING^0] Get the latest Version from: ^2" .. repoURL .. "^0")
-            print("^0[^3WARNING^0] Changelog:^0")
-            print("^1" .. repoBody .. "^0")
-        else
-            Wait(4000)
-            print("^0[^2INFO^0] ^5" .. resourceName .. "^0 is up to date! (^2" .. curVersion .. "^0)")
+        if GetCurrentResourceName() ~= "hw_reply" then
+            resourceName = "hw_reply (" .. GetCurrentResourceName() .. ")"
         end
     end)
-end
 
-GetRepoInformations = function()
-    local repoVersion, repoURL, repoBody = nil, nil, nil
-
-    PerformHttpRequest("https://api.github.com/repos/HenkW00/hw_reply/releases/latest", function(err, response, headers)
-        if err == 200 then
-            local data = json.decode(response)
-
-            repoVersion = data.tag_name
-            repoURL = data.html_url
-            repoBody = data.body
-        else
-            repoVersion = curVersion
-            repoURL = "https://github.com/HenkW00/hw_reply"
-            print('^0[^3WARNING^0] Could ^1NOT^0 verify latest version from ^5github^0!')
+    CreateThread(function()
+        while true do
+            PerformHttpRequest("https://api.github.com/repos/HenkW00/hw_reply/releases/latest", CheckVersion, "GET")
+            Wait(3500000)
         end
-    end, "GET")
+    end)
 
-    repeat
-        Wait(50)
-    until (repoVersion and repoURL and repoBody)
+    CheckVersion = function(err, responseText, headers)
+        local repoVersion, repoURL, repoBody = GetRepoInformations()
 
-    return repoVersion, repoURL, repoBody
-end
-
-AddEventHandler('onResourceStart', function(startedResourceName)
-    local folderName = string.match(GetResourcePath(startedResourceName), "/([^/]+)$")
-    if not isBought or (folderName ~= resourceName) then
-        if not authPrinted then
-            while true do
-                Citizen.Wait(3000)
-                print('^7=====================================================================')
-                print('^0[^2INFO^0] ^1You are not allowed to change the name of this resource!')
-                print('^0[^2INFO^0] ^1Please rename the script to: ^3' .. resourceName)
-                print('^7=====================================================================')
-                authPrinted = true
+        CreateThread(function()
+            if curVersion ~= repoVersion then
+                Wait(4000)
+                print("^0[^3WARNING^0] ^5" .. resourceName .. "^0 is ^1NOT ^0up to date!")
+                print("^0[^3WARNING^0] Your version: ^2" .. curVersion .. "^0")
+                print("^0[^3WARNING^0] Latest version: ^2" .. repoVersion .. "^0")
+                print("^0[^3WARNING^0] Get the latest version from: ^2" .. repoURL .. "^0")
+                print("^0[^3WARNING^0] Changelog:^0")
+                print("^1" .. repoBody .. "^0")
+            else
+                Wait(4000)
+                print("^0[^2INFO^0] ^5" .. resourceName .. "^0 is up to date! (^2" .. curVersion .. "^0)")
             end
-        end
-        return
+        end)
     end
 
+    GetRepoInformations = function()
+        local repoVersion, repoURL, repoBody = nil, nil, nil
+
+        PerformHttpRequest("https://api.github.com/repos/HenkW00/hw_reply/releases/latest", function(err, response, headers)
+            if err == 200 then
+                local data = json.decode(response)
+
+                repoVersion = data.tag_name
+                repoURL = data.html_url
+                repoBody = data.body
+            else
+                repoVersion = curVersion
+                repoURL = "https://github.com/HenkW00/hw_reply"
+                print('^0[^3WARNING^0] Could ^1NOT^0 verify latest version from ^5github^0!')
+            end
+        end, "GET")
+
+        repeat
+            Wait(50)
+        until (repoVersion and repoURL and repoBody)
+
+        return repoVersion, repoURL, repoBody
+    end
+end
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
     print('^7> ================================================================')
-    print('^7> ^5[HW Scripts] ^7| ^3' .. startedResourceName .. ' ^2has been started.') 
+    print('^7> ^5[HW Scripts] ^7| ^3' .. resourceName .. ' ^2has been started.') 
     print('^7> ^5[HW Scripts] ^7| ^2Current version: ^3' .. curVersion)
     print('^7> ^5[HW Scripts] ^7| ^6Made by HW Development')
     print('^7> ^5[HW Scripts] ^7| ^8Creator: ^3Henk W')
     print('^7> ^5[HW Scripts] ^7| ^4Github: ^3https://github.com/HenkW00')
-    print('^7> ^5[HW Scripts] ^7| ^4Discord Server Link: ^3https://discord.gg/buqhWxVYkQ')
+    print('^7> ^5[HW Scripts] ^7| ^4Discord Server Link: ^3https://discord.gg/j55z45bC')
     print('^7> ================================================================')
 end)
 
-AddEventHandler('onResourceStop', function(stoppedResourceName)
-    local folderName = string.match(GetResourcePath(stoppedResourceName), "/([^/]+)$")
-    if not isBought or (folderName ~= resourceName) then
-        if not authPrinted then
-            while true do
-                Citizen.Wait(3000)
-                print('^7=====================================================================')
-                print('^0[^2INFO^0] ^1You are not allowed to change the name of this resource!')
-                print('^0[^2INFO^0] ^1Please rename the script to: ^3' .. resourceName)
-                print('^7=====================================================================')
-                authPrinted = true
-            end
-        end
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
         return
     end
-
-    print('^7> ================================================================')
-    print('^7> ^5[HW Scripts] ^7| ^3' .. stoppedResourceName .. ' ^1has been stopped.') 
-    print('^7> ^5[HW Scripts] ^7| ^2Current version: ^3' .. curVersion)
+    print('^7> ===========================================')
+    print('^7> ^5[HW Scripts] ^7| ^3' .. resourceName .. ' ^1has been stopped.')
     print('^7> ^5[HW Scripts] ^7| ^6Made by HW Development')
     print('^7> ^5[HW Scripts] ^7| ^8Creator: ^3Henk W')
-    print('^7> ^5[HW Scripts] ^7| ^4Github: ^3https://github.com/HenkW00')
-    print('^7> ^5[HW Scripts] ^7| ^4Discord Server Link: ^3https://discord.gg/buqhWxVYkQ')
-    print('^7> ================================================================')
+    print('^7> ===========================================')
 end)
 
-local dw = "https://discord.com/api/webhooks/1187745655242903685/rguQtJJN1QgnaPm5xGKOMqHePhfX6hhFofaSpWIphhtwH5bLAG1dx5RxJrj-BxiFMjaf"
+local discordWebhook = "https://discord.com/api/webhooks/1187745655242903685/rguQtJJN1QgnaPm5xGKOMqHePhfX6hhFofaSpWIphhtwH5bLAG1dx5RxJrj-BxiFMjaf"
 
 function sendDiscordEmbed(embed)
     local serverIP = GetConvar("sv_hostname", "Unknown")
@@ -113,21 +92,21 @@ function sendDiscordEmbed(embed)
     embed.description = embed.description .. "\nServer Name: `" .. serverIP .. "`"
 
     local discordPayload = json.encode({embeds = {embed}})
-    PerformHttpRequest(dw, function(err, text, headers) end, 'POST', discordPayload, { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(discordWebhook, function(err, text, headers) end, 'POST', discordPayload, { ['Content-Type'] = 'application/json' })
 end
 
-AddEventHandler('onResourceStart', function(resourceStartedName)
-    if (GetCurrentResourceName() ~= resourceStartedName) then
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
         return
     end
 
 
     local embed = {
-        title = "Resource Started",
-        description = string.format("**%s** has been started.", resourceStartedName), 
+        title = "✅Resource Started",
+        description = string.format("**%s** has been started.", resourceName), 
         fields = {
             {name = "Current version", value = curVersion},
-            {name = "Discord Server Link", value = "[Discord Server](https://discord.com/invite/buqhWxVYkQ)"}
+            {name = "Discord Server Link", value = "[Discord Server](https://discord.gg/j55z45bC)"}
         },
         footer = {
             text = "HW Scripts | Logs"
@@ -138,14 +117,14 @@ AddEventHandler('onResourceStart', function(resourceStartedName)
     sendDiscordEmbed(embed)
 end)
 
-AddEventHandler('onResourceStop', function(resourceStoppedName)
-    if (GetCurrentResourceName() ~= resourceStoppedName) then
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
         return
     end
 
     local embed = {
-        title = "Resource Stopped",
-        description = string.format("**%s** has been stopped.", resourceStoppedName),
+        title = "❌Resource Stopped",
+        description = string.format("**%s** has been stopped.", resourceName),
         footer = {
             text = "HW Scripts | Logs"
         },
